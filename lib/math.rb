67d4153967c1283b5d8b34e9aaf1e5df653161c3
Lindsay M <lindsaymecher@gmail.com>
2019-11-09 08:56:35 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,26 @@ sum
 end
 
 def subtraction(num1, num2)
-
+difference = num1 - num2
+difference
 end
 
 def division(num1, num2)
-
+quotient = num1 / num2
+quotient
 end
 
 def multiplication(num1, num2)
-
+product = num1 * num2
+product
 end
 
 def modulo(num1, num2)
-
+remainder = num1%num2
+remainder
 end
 
 def square_root(num)
-
+root = Math.sqrt(num)
+root
 end
